50ce23167010ee07b787a37487323c1564b1bafa
inguk <inguk@blast-team.com>
2023-10-24 18:58:25 +0900

    feat. 구성완료료

--- src/pages/Home/HomeList.tsx
+++ src/pages/Home/HomeList.tsx
@@ -5,32 +5,11 @@ import { TextCard } from "@src/components/TextCard";
 import { Progressbar } from "@src/components/Progressbar";
 
 export const HomeList = () => {
-  // <div>
-  //   <div className="my-5 ticky top-0 flex h-screen flex-col">
-  //     <Divider dividerOption="Work" />
-  //     <Banner setShowModal={setShowModal} />
-  //   </div>
-  //   <div className="sticky top-0 flex h-screen flex-col bg-gray-100">
-  //     {PlotCardData.map((item) => (
-  //       <TextCard key={item.id} data={item} />
-  //     ))}
-  //   </div>
-
-  //   {/* <div className="gap-2 grid grid-cols-3">
-  //   {ProjectCardData.map((item) => (
-  //     <Card key={item.id} data={item} />
-  //   ))}
-  // </div> */}
-  //   {showModal && <Modal setShowModal={setShowModal} />}
-  // </div>
-  // const ProjectCardData = useMemo(() => ProjectData, []);
   const PlotCardData = useMemo(() => PlotData, []);
 
   return (
-    // 기본width 제한 <div className="container my-6 mx-auto px-6 md:px-12 xl:px-32">
-    //
     <div className=" text-black gradient mt-12">
-      <div className="container flex justify-between mx-auto items-center px-12">
+      <div className="container flex justify-between mx-auto items-center px-4 lg:px-12">
         <div className="text-3xl font">Frontend Developer</div>
         <div className="flex flex-row">
           <div className="flex flex-row items-center">
@@ -44,7 +23,7 @@ export const HomeList = () => {
         </div>
       </div>
       <Progressbar />
-      <div className="container px-12 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div className="container px-4 lg:px-12 mx-auto flex flex-wrap flex-col md:flex-row items-center">
         <div className="flex flex-col w-full md:w-2/5 justify-center items-start md:text-left leading-9 ">
           <h1 className="lg:text-5xl  font-bold leading-tight text-3xl mb-3">
             새로움과 도전을 즐기는
@@ -80,12 +59,12 @@ export const HomeList = () => {
               <div className="mt-6 lg:mt-0 lg:mx-6 ">
                 <p className="text-xl text-blue-500 font-bold">Introduce</p>
                 <p className="block mt-4 text-lg  font-normal text-gray-600 hover:underline">
-                  웹 퍼블리싱과 프론트엔드 부트캠프 과정을 수료하면서 HTML과 CSS를 시멘틱하게 다룰수 있게 되었으며,
-                  사람들과 함께 협업하며 성장하기위해 리액트와 타입스크립트를 이용한 프로젝트와 cs지식을 위한 스터디를
-                  4개 진행한 경험이 있습니다.
+                  웹 퍼블리싱과 프론트엔드 부트캠프 과정을 수료하면서 HTML과 CSS를 시멘틱하게 사용이 가능하며 리액트와
+                  타입스크립트를 다루는 데 익숙합니다, 사람들과 함께 협업하며 성장하기위해 리액트와 타입스크립트를
+                  이용한 프로젝트와 cs지식을 위한 스터디를 4개 진행한 경험이 있습니다.
                 </p>
                 <p className="block mt-4 text-lg  font-normal text-gray-600 hover:underline">
-                  개발자가 되기 전에는 항공정비사와 모바일 인력소장으로 활동하며 다양한 경험을 쌓았습니다. 그런 경험들을
+                  개발자가 되기 전에는 항공정비사와 모바일 인력소장으로 활동하며 다양한 경험을 쌓았습니다. 이런 경험들을
                   바탕으로 신뢰받는 개발자가 되겠습니다.
                 </p>
               </div>
