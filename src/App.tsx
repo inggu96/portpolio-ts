f69d9eae6b7ee37e32739f6c5e4a3ebbef59cac0
inguk <inguk@blast-team.com>
2023-12-27 15:21:16 +0900

    fix.빌드에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { HomeList } from "./pages/Home/HomeList";
-import { Layout } from "./pages/Layout/Layout";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeList />} />
-      <Route path="/layout" element={<Layout />} />
     </Routes>
   );
 }
